e4c3c3a26474c3bc19b81617b6f53e5f93a01a3a
David Graunke <graunked@gmail.com>
2014-10-09 18:19:58 -0700

    Add routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,17 @@
 'use strict';
 
 angular
-  .module('scrollviewTestApp', ['famous.angular']);
+  .module('scrollviewTestApp', ['famous.angular', 'ngRoute'])
+  .config(function($routeProvider, $locationProvider) {
+    $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl',
+    })
+    .when('/stack', {
+      templateUrl: 'stack.html',
+      controller: 'StackController'
+    });
+
+    // configure html5 to get links working on jsfiddle
+  });
