4d9a76aa9833b3002efcdc43e2186cfec696404b
David Graunke <graunked@gmail.com>
2014-10-09 17:11:45 -0700

    Add scaling

--- app/scripts/controllers/scroll.js
+++ app/scripts/controllers/scroll.js
@@ -4,13 +4,25 @@ angular.module('scrollviewTestApp')
   .controller('ScrollCtrl', function ($scope, $timeline) {
 
     $scope.distance = function(i) {
-      return $scope.scrollViewDistance(i, 100);
+      return $scope.scrollViewDistance(i, 120);
     };
 
     $scope.scale = $timeline([
       [-1, [0.5, 0.5]],
       [0, [1, 1]],
       [1, [0.5, 0.5]],
+    ])
+
+    $scope.rotate = $timeline([
+      [-1, -Math.PI / 10],
+      [0, 0],
+      [1, Math.PI / 10],
+    ])
+
+    $scope.translate = $timeline([
+      [-1, [0, 50, 0]],
+      [0, [0, 0, 0]],
+      [1, [0, 50, 0]],
     ])
 
   });
